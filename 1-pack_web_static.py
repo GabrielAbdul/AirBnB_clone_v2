3f5e4a4888e3fb09942801a6b4e061b0422a66f2
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-17 20:02:22 +0000

    initial commit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Fabric script that generates a .tgz archive from the contents of the web_static folder'''
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    ''''''
+    archive = 'web_static_{}.tgz'.format(datetime.now().strftime('%Y%m%d%H%M%S'))
+    local('mkdir -p versions')
+
+    try:
+        local('tar -cvzf {} web_static'.format(archive))
+        return "versions/{}".format(archive)
+    except:
+        return None
