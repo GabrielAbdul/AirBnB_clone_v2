c2fff9fb3d1d8506c3318c30b14b0d997f84f412
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-18 00:49:45 +0000

    initial commit

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,4 @@
+exec {'wget'
+    provider => 'shell',
+    command  => 'wget https://raw.githubusercontent.com/GabrielAbdul/AirBnB_clone_v2/master/0-setup_web_static.sh && chmod +x 0-setup_web_static.sh && ./0-setup_web_static.sh'
+}
