79e1e0d8514bf9130d7be76581feb2b8f444dd97
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-18 00:15:30 +0000

    pep8 fixes

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,9 +3,9 @@
 
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
-
 web01, web02 = '104.196.165.127', '34.230.11.25'
 env.hosts = [web01, web02]
+
 
 def deploy():
     '''creates and distributes an archive to web servers'''
