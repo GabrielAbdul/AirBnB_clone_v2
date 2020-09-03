e12342bedf231038ef1a61e94d0815436656664a
GabrielAbdul <gabrielabdul1@gmail.com>
2020-09-03 00:55:24 +0000

    used state object insteqad of state string

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,7 +5,6 @@
 from flask import Flask
 from flask import render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
@@ -19,7 +18,7 @@ def teardown_method(self):
 @app.route('/states_list', strict_slashes=False)
 def show_states_list_html():
     '''displays states_list html page'''
-    states = storage.all('State')
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 if __name__ == '__main__':
